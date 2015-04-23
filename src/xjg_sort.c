c8eccdd177366521c0df20fd76ad3ff6ab4df37e
xujianguo <450846733@qq.com>
2015-04-23 22:31:23 +0800

    add the bubble sort

--- src/xjg_sort.c
+++ src/xjg_sort.c
@@ -22,3 +22,31 @@ void xjg_sort_insert(void *array, uint_t size, xjg_compare compare, uint_t start
 	}
 }
 
+/**
+ * 冒泡排序：
+ *		优化一：加入了hashChange标志，如果标记没有被改变，证明序列中已经排序好来，不必要进行下次操作
+ *		优化二：加入了mark和index标记，用于确定最终改变行的位置，下次就从那个位置开始，因为可能之前的已经排序好
+ */
+void xjg_sort_bubble(void *array, uint_t size, xjg_compare compare, uint_t start, uint_t end) {
+	uchar_t *temp = (uchar_t*)malloc(size);
+	int i, j, mark, hasChange, index;
+	mark = end - 1;
+	hasChange = 0;
+	index = 0;
+
+	for(i = 1; i <= end-start; i++) {
+		for(j = 0; j <= mark; j++) {
+			if(compare(array+j*size, array+(j+1)*size) > 0) {
+				memcpy(temp, array+j*size, size);
+				memcpy(array+j*size, array+(j+1)*size, size);
+				memcpy(array+(j+1)*size, temp, size);
+				hasChange = 1;
+				index = j;
+			}
+		}
+		if(hasChange == 0)
+			break;
+		mark = index;
+	}
+}
+
--- src/xjg_sort.h
+++ src/xjg_sort.h
@@ -20,5 +20,15 @@
  */
 void xjg_sort_insert(void *array, uint_t size, xjg_compare compare, uint_t start, uint_t end);
 
+/**
+ * 冒泡排序
+ * @param array 待排序数组指针
+ * @param size 数组元素大小字节数
+ * @param compare 比较函数指针
+ * @param start 开始位置
+ * @param end 结束位置
+ */
+void xjg_sort_bubble(void *array, uint_t size, xjg_compare compare, uint_t start, uint_t end);
+
 #endif
 
